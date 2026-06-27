c600d554e1b2c34eb8cbe52f1bdeae77743ef6a3
salmanropleh <shafiropleh@gmail.com>
2026-06-28 01:31:58 +0300

    Revert "Add public newsletter archive endpoints (no auth required)"
    This reverts commit 79cd6d75ade93fe568913acbe7182a9b78ae453a.

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -4,11 +4,10 @@ URL patterns for newsletter app.
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import NewsletterViewSet, NewsletterSubscriptionViewSet, NewsletterPublicViewSet
+from .views import NewsletterViewSet, NewsletterSubscriptionViewSet
 
 router = DefaultRouter()
 router.register(r'subscriptions', NewsletterSubscriptionViewSet, basename='subscription')
-router.register(r'newsletters/public', NewsletterPublicViewSet, basename='newsletter-public')
 router.register(r'newsletters', NewsletterViewSet, basename='newsletter')
 
 app_name = 'newsletter'
